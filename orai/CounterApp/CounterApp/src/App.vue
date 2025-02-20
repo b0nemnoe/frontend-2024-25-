97ffb2b9c06f05cfe22dbac7b03ddacf63c3679c
b0nemnoe <b0nemnoe@vasvari.org>
2025-02-20 12:13:15 +0100

    CounterApp kész (kinézet)

---
+++ orai/CounterApp/CounterApp/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+</script>
+
+<template>
+<header class="p-3 fs-4" style="background-color: #C7D2FE;">
+  My Counter
+</header>
+
+<div class="container d-flex flex-column align-items-center justify-content-center vh-100">
+  
+  <div class="text-center">
+      <h3>Counter value:</h3>
+      <h2>100</h2>
+    </div>
+
+  <div class="d-flex justify-content-between align-items-center w-50">
+    
+
+    <div class="minus d-flex flex-column gap-2">
+      <button class="btn btn-lg" style="background-color: #C7D2FE;">-</button>
+      <button class="btn btn-lg" style="background-color: #C7D2FE;">-</button>
+    </div>
+
+
+    
+
+
+    <div class="plus d-flex flex-column gap-2">
+      <button class="btn btn-lg" style="background-color: #C7D2FE;">+</button>
+      <button class="btn btn-lg" style="background-color: #C7D2FE;">+</button>
+    </div>
+    
+  </div>
+</div>
+
+
+
+</template>
+
+<style scoped>
+</style>
