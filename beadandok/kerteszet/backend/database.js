8cd472b8c295b3b33af16a56072f525f8d3060d9
b0nemnoe <b0nemnoe@vasvari.org>
2025-03-07 11:33:47 +0100

    javitas

--- beadandok/kerteszet/backend/database.js
+++ beadandok/kerteszet/backend/database.js
@@ -3,26 +3,21 @@ import sqlite3 from 'sqlite3'
 const database = new sqlite3.Database('./database.sqlite')
 
 async function initializeDatabase() {
-
-    await dbRun("DROP TABLE IF EXISTS plants")
     await dbRun(`
-        CREATE TABLE plants (
+        CREATE TABLE IF NOT EXISTS plants (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT,
-            perennial boolean,
+            perennial BOOLEAN,
             category TEXT,
-            price INTEGER )`)
+            price INTEGER
+        )
+    `)
 
-            const plants = [
-                { id:1, name: "Tulipán", perennial: false, category: "virág", price: 100 },
-                { id:2, name: "Rózsa", perennial: true, category: "virág", price: 200 },
-                { id:3, name: "Orgona", perennial: true, category: "bokor", price: 300 },
-                { id:4, name: "Fűz", perennial: true, category: "fa", price: 400 },
-                { id:5, name: "Tölgy", perennial: true, category: "fa", price: 500 }
-            ]
-
-    for (const plant of plants) {
-        await dbRun(`INSERT INTO plants (name, perennial, category, price) VALUES (?, ?, ?, ?)`, [plant.name, plant.perennial, plant.category, plant.price])
+    const existingPlants = await dbQuery("SELECT COUNT(*) as count FROM plants")
+    if (existingPlants[0].count === 0) {
+        for (const plant of plants) {
+            await dbRun(`INSERT INTO plants (name, perennial, category, price) VALUES (?, ?, ?, ?)`, [plant.name, plant.perennial, plant.category, plant.price])
+        }
     }
 }
 
